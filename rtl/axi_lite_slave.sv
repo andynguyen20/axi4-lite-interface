975462063747ebd083e1355490dbf101b0410501
andynguyen20 <andynguyenn20@gmail.com>
2025-12-23 22:28:13 -0500

    Almost finished write logic

--- rtl/axi_lite_slave.sv
+++ rtl/axi_lite_slave.sv
@@ -35,25 +35,36 @@ module axi_lite_slave(
     input logic s_axi_bready
     );
     
-    reg [31:0] rfile[3:0];
-    
-    logic [1:0] windex = s_axi_awaddr[3:2];  // 4 regs at 0x0,0x4,0x8,0xC
+    reg [31:0] reg_file[3:0];
     
     logic aw_hs;
     logic w_hs;
-    logic ar_hs;
-    logic r_hs;
     logic b_hs;
     
     assign aw_hs = s_axi_awvalid && s_axi_awready;
     assign w_hs = s_axi_wvalid && s_axi_wready;
-    assign ar_hs = s_axi_arvalid && s_axi_arready;
-    assign r_hs =s_axi_rvalid && s_axi_rready;
     assign b_hs = s_axi_bvalid && s_axi_bready;
     
     assign s_axi_awready = 1'b1; 
     assign s_axi_wready = 1'b1;
-    assign s_axi_arready = 1'b1;
+    
+    logic [31:0] awaddr_buf;
+    logic [31:0] wdata_buf;
+    logic [31:0] wdata_mask;
+    logic [3:0] wstrb_buf;
+    
+    logic aw_pending;
+    logic w_pending;
+    
+    logic [1:0] aw_index;
+    assign aw_index = awaddr_buf[3:2];  // 4 regs at 0x0,0x4,0x8,0xC
+    
+    always_comb begin                       //write strobe logic
+        wdata_mask = 32'b0;
+        for(int i = 0; i < 4; i++) begin
+            wdata_mask[i*8 +: 8] = {8{wstrb_buf[i]}};
+        end
+     end
     
     always_ff @(posedge aclk) begin
         if(~aresetn) begin
@@ -61,12 +72,23 @@ module axi_lite_slave(
             s_axi_rdata <= 32'b0;
             s_axi_rresp <= 2'b0;
             s_axi_rvalid <= 1'b0;
-            s_axi_awready <= 1'b0;
-            s_axi_wready <= 1'b0;
             s_axi_bresp <= 2'b00;
             s_axi_bvalid <= 1'b0;
+            aw_pending <= 1'b0;
+            w_pending <= 1'b0;
         end
         else begin
+            if(aw_hs) begin                 // write logic
+                awaddr_buf <= s_axi_awaddr;
+                aw_pending <= 1'b1;
+            end
+            if(w_hs) begin
+                wdata_buf <= s_axi_wdata;
+                wstrb_buf <= s_axi_wstrb;
+                w_pending <= 1'b1;
+            end
+            if(aw_pending && w_pending && !s_axi_bvalid) begin
+            end
         end
      end
             
